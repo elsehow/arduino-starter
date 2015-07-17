c3eb2e31dd3c8902fc05ac58d88b00fdbe51cdae
elsehow <ffff@localhost.(none)>
2015-07-16 22:45:59 -0700

    working on my uno

--- main.ino
+++ main.ino
@@ -1,12 +1,22 @@
-// blink pin 13
+#include <SoftwareSerial.h>
 
 void setup() {                
     pinMode(13, OUTPUT);     
+    Serial.begin(9600);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+
+  byte cmd = 0;
+  boolean haveEverHeard = false;
+
+  if (Serial.available() > 0) {
+    cmd = Serial.read();
+    haveEverHeard = true;
+  }
+
+  // if (cmd == 'G') {
+  if (haveEverHeard == true) {
+  	digitalWrite(13, HIGH);
+  }
 }
